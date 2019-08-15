4867d3d464da96cfce425dd367effe2fa9cbf52c
neetika-Sri <neetika.srivastava@gmail.com>
2019-08-15 15:12:12 +1200

    Add package a

---
+++ a/a.go
@@ -0,0 +1,10 @@
+package main 
+ import (
+"github.com/NeetikaHome/go_submod/b"
+"fmt"
+)
+const Name = b.Name
+
+func main() {
+fmt.Println(Name)
+}
---
+++ a/go.mod
@@ -0,0 +1,5 @@
+module github.com/NeetikaHome/go_submod/a
+
+go 1.12
+
+require github.com/NeetikaHome/go_submod/b v0.1.1
